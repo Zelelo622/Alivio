89e1df6ae0221bc8e485e8ffd61ad9e051042e34
Egor Kirin <egorcom27@gmail.com>
2022-06-28 22:43:04 +0300

    adaptive story

--- js/main.js
+++ js/main.js
@@ -7,4 +7,36 @@ mobileNavBtn.addEventListener('click', function () {
     mobileNavIcon.classList.toggle('active');
     mobileNav.classList.toggle('active');
     document.body.classList.toggle('no-scroll');
-})
+});
+
+/* VIDEO */
+const videoBtn = document.querySelector('#video-story-btn');
+const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+const videoOverlay = document.querySelector('#video-story-overlay');
+const videoFile = document.querySelector('#video-story');
+
+videoBtn.addEventListener('click', function () {
+
+    function toggleOverlay(event) {
+        if (event.type === "mouseleave") {
+            videoOverlay.classList.add("hidden");
+        } else {
+            videoOverlay.classList.remove("hidden");
+        }
+    }
+
+    if (videoFile.paused) {
+        videoFile.play();
+        videoBtnIcon.src = './img/pause-white.svg';
+
+        videoOverlay.onmouseleave = toggleOverlay;
+        videoOverlay.onmouseenter = toggleOverlay;
+
+    } else {
+        videoFile.pause();
+        videoBtnIcon.src = './img/play-white.svg';
+        videoOverlay.onmouseleave = null;
+        videoOverlay.onmouseenter = null;
+
+    }
+});
